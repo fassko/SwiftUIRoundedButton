4f4589f06cbfd97b2188cf507a66a289f045d3be
Kristaps Grinbergs <fassko@gmail.com>
2021-07-13 18:29:09 +0300

    first commit

---
+++ SwiftUIRoundedButton/DeliveryTimeButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  DeliveryTimeButtonView.swift
+//  SwiftUIRoundedButton
+//
+//  Created by Kristaps Grinbergs on 13/07/2021.
+//
+
+import SwiftUI
+
+struct DeliveryTimeButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DeliveryTimeButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        DeliveryTimeButtonView()
+    }
+}
