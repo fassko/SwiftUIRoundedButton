911faf7cde5209c26132ec105994df2729004717
Kristaps Grinbergs <fassko@gmail.com>
2021-07-13 17:31:33 +0300

    Initial Commit

---
+++ SwiftUIRoundedButton/SwiftUIRoundedButtonApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIRoundedButtonApp.swift
+//  SwiftUIRoundedButton
+//
+//  Created by Kristaps Grinbergs on 13/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIRoundedButtonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
